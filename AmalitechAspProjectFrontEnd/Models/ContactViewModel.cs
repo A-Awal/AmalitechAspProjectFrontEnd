2856b30b3e558f8ca4ca7a638777ab5856377f43
A-Awal <abdulai.awal@amalitech.org>
2022-12-13 19:50:57 +0000

    left with Aesthetics

---
+++ AmalitechAspProjectFrontEnd/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmalitechAspProjectFrontEnd.Models
+{
+    public class ContactViewModel
+    {
+        [Display(Name="Name")]
+        public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string EmailAdress { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public int Phone { get; set; }
+        [Display(Name ="Your Message")]
+        public string YourMessage { get; set; }
+
+    }
+}
